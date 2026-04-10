56eac4caa6c4980b20ac505f43f0ce10bdd1e3fc
Daniel Code Master <daniel251107ribeiro@gmail.com>
2026-04-10 10:55:11 -0300

    Add basic arithmetic operations and user prompts
    Initial script for basic arithmetic operations and user prompts.

---
+++ 26_03_26_script.js
@@ -0,0 +1,23 @@
+// WebDev2603
+// (dentro script.js)
+
+// colocar em bloco de anotação para rodar cada um por vez, se não dá ruim
+
+// desafio em aula:  
+let n1 = parseInt(prompt('digita um numero'));
+let n2 = parseInt(prompt('digita outro numero '));
+
+console.log(`a soma será de: ${n1 + n2} `);
+alert(`a multipicação será de: ${n1 * n2}`);
+
+ /* let nome = prompt('digita seu nome:'); 
+console.log('Bem vindo!');
+console.log(`Olá, ${nome}! `); 
+
+ alert('bem vindo')
+ let nome = prompt('digita seu nome:'); 
+ alert(`Olá, ${nome}! `);
+
+let idade =  parseInt(prompt('fala tua idade:'));
+alert(`Você terá ${IdadeInt +  30} daqui 30 anos, tá velho!`);
+  */
